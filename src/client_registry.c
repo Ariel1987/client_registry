4e4257b7fd4fd2bf7c2ff010b68cc5efbe88da39
Ariel <arielpiresdealmeida@gmail.com>
2021-01-26 18:17:44 -0300

    Continue Modularizing

--- src/client_registry.c
+++ src/client_registry.c
@@ -29,7 +29,9 @@ struct Registry *readFile(int size);
 int chooseClient();
 int removeClient();
 int updateExpenses();
-
+int zeroExpenses();
+int bestBuyer();
+int clientExpenses();
 int editAndShowList();
 
 
@@ -129,6 +131,8 @@ int addClient() {
 	fclose(fp);
 	fp = NULL;
 
+	printf("\nClients added successfully!\n");
+	startProgram();
 	return 0;
 }
 
@@ -196,6 +200,7 @@ int removeClient() {
 	fclose(fp);
 	fp = NULL;
 
+	editAndShowList();
 	return 0;
 }
 
@@ -223,6 +228,68 @@ int updateExpenses() {
 	fclose(fp);
 	fp = NULL;
 
+	editAndShowList();
+	return 0;
+}
+
+int zeroExpenses() {
+
+	int size = lineCounter();
+	struct Registry *clients = readFile(size);
+
+	int i;
+	for (i = 0; i < size; i++) {
+		clients[i].expenses = 0.0;
+	}
+
+	FILE *fp = openFile(FILENAME, "w");
+
+	for(i = 0; i < size; i++) {
+		fprintf(fp, "%s %d %.2f\n", clients[i].name, clients[i].yearOfBirth, clients[i].expenses);
+	}
+	fclose(fp);
+	fp = NULL;
+
+	editAndShowList();
+	return 0;
+}
+
+int bestBuyer() {
+
+	int size = lineCounter();
+	struct Registry *clients = readFile(size);
+
+	int i;
+	for (i = 0; i < size; i++) {
+		if (clients[0].expenses < clients[i].expenses) {
+			clients[0].expenses = clients[i].expenses;
+			clients[0].yearOfBirth = clients[i].yearOfBirth;
+			strncpy(clients[0].name, clients[i].name, 30);
+		}
+	}
+	printf("%s %d %.2f\n", clients[0].name, clients[0].yearOfBirth, clients[0].expenses);
+
+	editAndShowList();
+	return 0;
+}
+
+int clientExpenses() {
+
+	int size = lineCounter();
+	struct Registry *clients = readFile(size);
+
+	printf("**********List of Clients**********\n");
+
+	int i, client;
+	for(i = 0; i < size; i++) {
+		printf("%d- %s\n", i + 1, clients[i].name);
+	}
+	printf("Insert client's number: ");
+	scanf("%d", &client);
+
+	printf("\n%s's expenses were: %.2f\n", clients[client - 1].name, clients[client - 1].expenses);
+
+	editAndShowList();
 	return 0;
 }
 
@@ -236,6 +303,8 @@ int editAndShowList() {
 	printf("3- Zero clients' expenses\n");
 	printf("4- Show best buyer\n");
 	printf("5- Show a client's expenses\n");
+	printf("6- Return to menu\n");
+	printf("7- Exit\n");
 	printf("Option: ");
 	scanf("%d", &option);
 
@@ -246,163 +315,26 @@ int editAndShowList() {
 	case 2:
 		updateExpenses();
 		break;
+	case 3:
+		zeroExpenses();
+		break;
+	case 4:
+		bestBuyer();
+		break;
+	case 5:
+		clientExpenses();
+		break;
+	case 6:
+		startProgram();
+		break;
+	case 7:
+		exit(0);
+		break;
+	default:
+		printf("\nInexistent choice");
+		editAndShowList();
+		break;
 	}
 
 	return 0;
 }
-
-
-
-	/*
-	FILE *fp = NULL;
-
-	int size = lineCounter();
-	Registry clients[size];
-	char token[121];
-	char * item;
-	int i = 0, option = 0;
-	int client;
-	float expense;
-
-	fp = fopen(FILENAME, "r");
-
-	if (fp == NULL)
-	{
-		perror("Error opening file");
-		return -1;
-	}
-
-	while (fgets(token, 120, fp))
-	{
-		item = strtok(token, " ");
-		strncpy(clients[i].name, item, sizeof(clients[i].name));
-
-		item = strtok(NULL, " ");
-		clients[i].yearOfBirth = atoi(item);
-
-		item = strtok(NULL, " ");
-		clients[i].expenses = atof(item);
-
-		i++;
-	}
-	fclose(fp);
-	fp = NULL;
-
-	printf("Choose one of the options bellow\n");
-	printf("1- Remove client or update his/her expenses\n");
-	printf("2- Zero client's expenses\n");
-	printf("3- Show best buyer\n");
-	printf("4- Show a client's expenses\n");
-	printf("Option: ");
-	scanf("%d", &option);
-
-
-	switch(option)
-	{
-	case 1:
-		printf("**********List of Clients**********\n");
-		for(i = 0; i < size; i++)
-		{
-			printf("%d- %s\n", i + 1, clients[i].name);
-		}
-
-		printf("Insert the client's number: ");
-		scanf("%d", &client);
-
-		printf("1- Delete client\n");
-		printf("2- Update client's expenses\n");
-		printf("Option: ");
-		scanf("%d", &option);
-
-		if(option == 1)
-		{
-			for(i = client; i < size; i++)
-			{
-				clients[i - 1] = clients[i];
-			}
-			size--;
-
-			fp = fopen(FILENAME, "w");
-
-			for(i = 0; i < size; i++)
-			{
-				fprintf(fp, "%s %d %.2f\n", clients[i].name, clients[i].yearOfBirth, clients[i].expenses);
-			}
-			fclose(fp);
-			fp = NULL;
-		}
-		else if (option == 2)
-		{
-			printf("How much are the client's new expenses?: ");
-			scanf("%f", &expense);
-			for (i = 0; i < size; i++)
-			{
-				if (i + 1 == client)
-				{
-					clients[i].expenses = expense;
-				}
-			}
-			fp = fopen(FILENAME, "w");
-
-			for(i = 0; i < size; i++)
-			{
-				fprintf(fp, "%s %d %.2f\n", clients[i].name, clients[i].yearOfBirth, clients[i].expenses);
-			}
-			fclose(fp);
-			fp = NULL;
-		}
-		else
-		{
-			printf("Invalid option");
-		}
-		break;
-
-	case 2:
-		for (i = 0; i < size; i++)
-		{
-			clients[i].expenses = 0.0;
-		}
-		fp = fopen(FILENAME, "w");
-
-		for(i = 0; i < size; i++)
-		{
-			fprintf(fp, "%s %d %.2f\n", clients[i].name, clients[i].yearOfBirth, clients[i].expenses);
-		}
-		fclose(fp);
-		fp = NULL;
-
-		break;
-
-	case 3:
-		for (i = 0; i < size; i++)
-		{
-			if (clients[0].expenses < clients[i].expenses)
-			{
-				clients[0].expenses = clients[i].expenses;
-				clients[0].yearOfBirth = clients[i].yearOfBirth;
-				strncpy(clients[0].name, clients[i].name, 30);
-			}
-		}
-		printf("%s %d %.2f\n", clients[0].name, clients[0].yearOfBirth, clients[0].expenses);
-		break;
-
-	case 4:
-		printf("**********List of Clients**********\n");
-		for(i = 0; i < size; i++)
-		{
-			printf("%d- %s\n", i + 1, clients[i].name);
-		}
-		printf("Insert client's number: ");
-		scanf("%d", &client);
-
-		printf("\n%s's expenses were: %.2f\n", clients[client - 1].name, clients[client - 1].expenses);
-
-		break;
-
-	default:
-		printf("Invalid option\n");
-	}
-
-	return 0;
-	*/
-
